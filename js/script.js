3b0608da319a7d6764a6441824dee3c4b17bd0f4
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-08-31 09:47:29 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -7,39 +7,43 @@ btnGenera.addEventListener("click",
         var km = parseInt(document.getElementById("km").value);
         var fascia = document.getElementById("fascia").value;
         // Calcolo il prezzo in base alla fascia d'età
-        var prezzo = km * 0.21;
-        var sconto = 0;
-        var offerta;
-        if (fascia == "minorenne") {
-            //sconto -20%
-            offerta = "Offerta Minorenni";
-            sconto = prezzo * 0.2;
-        } else if (fascia == "over") {
-            //sconto -40%
-            offerta = "Offerta Silver";
-            sconto = prezzo * 0.4;
-        } else {
-            offerta = "Biglietto Standard";
+        if ( nome != "" && !isNaN(km) && km > 0 && fascia != ""){
+            var prezzo = km * 0.21;
+            var sconto = 0;
+            var offerta;
+            if (fascia == "minorenne") {
+                //sconto -20%
+                offerta = "Sconto Minorenne";
+                sconto = prezzo * 0.2;
+            } else if (fascia == "over") {
+                //sconto -40%
+                offerta = "Sconto Silver";
+                sconto = prezzo * 0.4;
+            } else {
+                offerta = "Biglietto Standard";
+            }
+            prezzo -= sconto;
+
+            // creo funzione per generare numeri random in intervallo
+            function between(min, max) {  
+                return Math.floor(
+                Math.random() * (max - min) + min
+                );
+            }
+            // stampo i dati
+            document.getElementById("nome-passeggero").innerHTML = nome;
+            document.getElementById("offerta").innerHTML= offerta;
+            document.getElementById("carrozza").innerHTML = between(1, 9);
+            document.getElementById("cod-cp").innerHTML = between(1, 9999);
+            document.getElementById("costo").innerHTML = prezzo.toFixed(2) + "€";
+
+            // mostro il biglietto
+
+            document.getElementById("biglietto").classList.add("open");
+        }      
+        else {
+            alert("Errore nella compilazione. Ricarica la pagina.")
         }
-        prezzo -= sconto;
-
-        // creo funzione per generare numeri random in intervallo
-        function between(min, max) {  
-            return Math.floor(
-              Math.random() * (max - min) + min
-            )
-          }
-        // stampo i dati
-        document.getElementById("nome-passeggero").innerHTML = nome;
-        document.getElementById("offerta").innerHTML= offerta;
-        document.getElementById("carrozza").innerHTML = between(1, 9);
-        document.getElementById("cod-cp").innerHTML = between(1, 9999);
-        document.getElementById("costo").innerHTML = prezzo.toFixed(2);
-
-        // mostro il biglietto
-
-        document.getElementById("biglietto").classList.add("open");
-        
     }
 );
 
